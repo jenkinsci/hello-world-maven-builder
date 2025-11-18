fa314db744210837032f70d5f01c707a556bf752
Alexander Brandes <mc.cache@web.de>
2025-11-18 15:40:47 +0100

    Remove Java 11 from test matrix
    Signed-off-by: Alexander Brandes <mc.cache@web.de>

--- src/test/java/io/jenkins/tools/MainTest.java
+++ src/test/java/io/jenkins/tools/MainTest.java
@@ -1,13 +1,13 @@
 package io.jenkins.tools;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class MainTest {
+class MainTest {
 
     /**
      * Rigourous Test :-)
